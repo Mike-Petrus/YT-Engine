0bae0057b149661d3582812367e8015818ac5269
Mike Petrus <Petrus@ucsb.edu>
2024-07-10 20:38:21 -0700

    Camera bug fixed

--- src/Camera/Camera.cpp
+++ src/Camera/Camera.cpp
@@ -6,8 +6,8 @@ void Camera::Update(float dt){
     
     if (m_Target != nullptr){
 
-        m_ViewBox.x = m_Target->X = SCREEN_WIDTH/2;
-        m_ViewBox.y = m_Target->Y = SCREEN_HEIGHT/2;
+        m_ViewBox.x = m_Target->X - SCREEN_WIDTH/2;
+        m_ViewBox.y = m_Target->Y - SCREEN_HEIGHT/2;
 
         if (m_ViewBox.x < 0){
             m_ViewBox.x = 0;
